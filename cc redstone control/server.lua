77c64726dfb4ef4488d0d3429ca873991163b1bd
gedeon <gedeon.jahns@web.de>
2021-05-24 15:47:11 +0200

    move into folder

---
+++ cc redstone control/server.lua
@@ -0,0 +1,34 @@
+-- mainloop
+if not fs.exists('state.lua') then
+    local initData = fs.open('state.lua', 'w')
+    initData.write('{}')
+    initData.close()
+end
+
+
+local data = fs.open('state.lua', 'r')
+State = textutils.unserialise(data)
+data.close()
+
+
+for side, bool in pairs(State.redstone) do
+    redstone.setOutput(side, bool)
+end
+
+rednet.open('left')
+
+
+print(os.getComputerID())
+while true do
+    local senderId, message, protocol = rednet.receive('rs_command')
+
+    redstone.setOutput(message[1], message[2])
+    
+    State.redstone[message[1]] = message[2]
+
+    local data = fs.open('state.lua', 'w')
+    data = textutils.serialise(State)
+    data.close()
+
+    print('redstone toggled')
+end
